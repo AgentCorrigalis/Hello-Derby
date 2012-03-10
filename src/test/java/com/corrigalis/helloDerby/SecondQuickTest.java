7df4873bf2b75a0811262fdef382e67256f01ea0
AgentCorrigalis <Agent.Corrigalis@gmail.com>
2012-03-10 15:48:21 +0000

    new quick test

--- src/test/java/com/corrigalis/helloDerby/SecondQuickTest.java
+++ src/test/java/com/corrigalis/helloDerby/SecondQuickTest.java
@@ -1,15 +1,13 @@
 package com.corrigalis.helloDerby;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-
-public class FirstQuickTest {
-
+public class SecondQuickTest {
+	
 	@Test
 	public void testString() {
 		assertTrue("Hello".equals(FirstClass.hello()));
 	}
-	
 }
